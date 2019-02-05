86d33844d94967415cf82749500aed25a8a8fa8c
LALLINA21 <47343636+LALLINA21@users.noreply.github.com>
2019-02-05 14:41:55 +0530

    Create pg3.py

---
+++ pg3.py
@@ -0,0 +1,5 @@
+x = input("enter the alp:")
+if(x=='A' or x=='a' or x=='E' or x=='e' or x=='I' or x=='i' or x=='O' or x=='o' or x=='U' or x=='u' ):
+  print(x, "Vowels")
+else:
+  print(x, "Consonant")
